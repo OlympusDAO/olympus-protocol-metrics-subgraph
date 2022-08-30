ec26d3398082f0a16e980ce5911b982bddf8e4af
Jem <0x0xjem@gmail.com>
2022-08-30 16:59:49 +0400

    Add more information to output. Do sanity check of liquid backing and market value.

--- query-test/index.ts
+++ query-test/index.ts
@@ -3,9 +3,8 @@
 import { ApolloClient, gql, HttpLink, InMemoryCache } from "@apollo/client/core";
 import { fetch } from "cross-fetch";
 import { writeFileSync, readFileSync, existsSync } from "fs";
-import { program, Option, InvalidArgumentError } from "commander";
+import { program, InvalidArgumentError } from "commander";
 
-const COMMANDS = ["latest-block", "test", "compare"];
 const COMPARISON_FILE = "comparison.json";
 
 type ComparisonResults = {
@@ -22,11 +21,22 @@ type ComparisonResults = {
     marketValue?: {
       base: number;
       branch: number;
+      diff: number;
       result: boolean;
     };
     liquidBacking?: {
       base: number;
       branch: number;
+      diff: number;
+      result: boolean;
+    };
+    liquidBackingCheck?: {
+      marketValue: number;
+      liquidBacking: number;
+      ohmInLiquidity: number;
+      ohmPrice: number;
+      illiquidAssets: number;
+      diff: number;
       result: boolean;
     };
   }
@@ -137,6 +147,62 @@ const getTokenRecords = async (subgraphId: string, block: string): Promise<Token
   return results.data.tokenRecords;
 };
 
+type TokenSupply = {
+  id: string;
+  block: string;
+  date: string;
+  token: string;
+  source: string;
+  balance: string;
+  type: string;
+}
+
+/**
+ * Fetches an array of TokenSupply objects from the GraphQL server.
+ * 
+ * @param subgraphId 
+ * @param block 
+ * @returns 
+ */
+ const getTokenSupplies = async (subgraphId: string, block: string): Promise<TokenSupply[]> => {
+  const query = `
+  {
+    tokenSupplies(where: {block: ${block}}) {
+      id
+      block
+      date
+      token
+      source
+      balance
+      type
+    }
+  }`;
+
+  console.info(`Fetching token supplies for subgraph id ${subgraphId} and block ${block}`);
+  const results = await performQuery(subgraphId, query);
+  return results.data.tokenSupplies;
+ };
+
+/**
+ * Fetches the OHM price from the GraphQL server.
+ * 
+ * @param subgraphId 
+ * @param block 
+ * @returns 
+ */
+ const getOhmPrice = async (subgraphId: string, block: string): Promise<number> => {
+  const query = `
+  {
+    protocolMetrics(first: 1, where: {block: ${block}}) {
+      ohmPrice
+    }
+  }`;
+
+  console.info(`Fetching OHM price for subgraph id ${subgraphId} and block ${block}`);
+  const results = await performQuery(subgraphId, query);
+  return parseFloat(results.data.protocolMetrics[0].ohmPrice);
+ };
+
 const writeTokenRecords = (subgraphId: string, testMode: string, block: string, comparisonFile: ComparisonResults): void => {
   const FILENAME = `records-${testMode}.json`;
   getTokenRecords(subgraphId, block).then((tokenRecords) => {
@@ -171,11 +237,13 @@ const DIFF_THRESHOLD = 1000;
 /**
  * Compares the market value from two branches, and adds the results to {comparisonFile}.
  * 
+ * Market value is calculated as: sum of the value (`value` property) of all TokenRecord objects for a given block.
+ * 
  * @param baseRecords 
  * @param branchRecords 
  * @param comparisonFile 
  */
-const compareMarketValue = (baseRecords: TokenRecord[], branchRecords: TokenRecord[], comparisonFile: ComparisonResults): void => {
+const compareMarketValueRecords = (baseRecords: TokenRecord[], branchRecords: TokenRecord[], comparisonFile: ComparisonResults): void => {
   // Perform sums
   console.info("Comparing market value");
   const baseMarketValue = calculateMarketValue(baseRecords);
@@ -187,6 +255,7 @@ const compareMarketValue = (baseRecords: TokenRecord[], branchRecords: TokenReco
   const marketValueResults = {
     base: baseMarketValue,
     branch: branchMarketValue,
+    diff: branchMarketValue - baseMarketValue,
     result:
       baseMarketValue - branchMarketValue < DIFF_THRESHOLD &&
       branchMarketValue - baseMarketValue < DIFF_THRESHOLD,
@@ -198,11 +267,13 @@ const compareMarketValue = (baseRecords: TokenRecord[], branchRecords: TokenReco
 /**
  * Compares the liquid backing from two branches, and adds the results to {comparisonFile}.
  * 
+ * Liquid backing is calculated as: sum of the value excluding OHM (`valueExcludingOhm` property) of all liquid TokenRecord objects (`isLiquid` = true) for a given block.
+ * 
  * @param baseRecords 
  * @param branchRecords 
  * @param comparisonFile 
  */
- const compareLiquidBacking = (baseRecords: TokenRecord[], branchRecords: TokenRecord[], comparisonFile: ComparisonResults): void => {
+ const compareLiquidBackingRecords = (baseRecords: TokenRecord[], branchRecords: TokenRecord[], comparisonFile: ComparisonResults): void => {
   // Perform sums
   console.info("Comparing liquid backing");
   const baseLiquidBacking = calculateLiquidBacking(baseRecords);
@@ -214,25 +285,63 @@ const compareMarketValue = (baseRecords: TokenRecord[], branchRecords: TokenReco
   const liquidBackingResults = {
     base: baseLiquidBacking,
     branch: branchLiquidBacking,
+    diff: branchLiquidBacking - baseLiquidBacking,
     result:
       baseLiquidBacking - branchLiquidBacking < DIFF_THRESHOLD &&
       branchLiquidBacking - baseLiquidBacking < DIFF_THRESHOLD,
   };
 
   comparisonFile.results.liquidBacking = liquidBackingResults;
+ }
+
+ /**
+  * Compares the market value and liquid backing records, using the following formula:
+  * 
+  * Market value = liquid backing + illiquid assets + # OHM in POL * OHM price
+  * 
+  * @param tokenRecords 
+  * @param supplyRecords 
+  * @param ohmPrice 
+  * @param comparisonFile 
+  */
+const doLiquidBackingCheck = (tokenRecords: TokenRecord[], supplyRecords: TokenSupply[], ohmPrice: number, comparisonFile: ComparisonResults): void => {
+  const marketValue = calculateMarketValue(tokenRecords);
+  const liquidBacking = calculateLiquidBacking(tokenRecords);
+  const ohmInLiquidity = supplyRecords.filter((tokenSupply) => tokenSupply.type == "Protocol-Owned Liquidity").reduce((previousValue, tokenSupply) => previousValue + +tokenSupply.balance, 0);
+  const illiquidAssetsValue = tokenRecords.filter((tokenRecord) => tokenRecord.isLiquid == false).reduce((previousValue, tokenRecord) => previousValue + +tokenRecord.value, 0);
+  const marketValueSum = liquidBacking + illiquidAssetsValue + ohmInLiquidity * ohmPrice;
+
+  comparisonFile.results.liquidBackingCheck = {
+    marketValue: marketValue,
+    liquidBacking: liquidBacking,
+    ohmInLiquidity: ohmInLiquidity,
+    ohmPrice: ohmPrice,
+    illiquidAssets: illiquidAssetsValue,
+    diff: marketValueSum - marketValue,
+    result: marketValue - marketValueSum < DIFF_THRESHOLD && marketValueSum - marketValue < DIFF_THRESHOLD,
+  };
 }
 
 const compareTokenRecords = (filenameBase: string, filenameBranch: string, comparisonFile: ComparisonResults): void => {
   console.info(`Comparing token records for base file ${filenameBase} and branch file ${filenameBranch}`);
 
-  // Read files, parse into JSON
+  // Read TokenRecord files, parse into JSON
   const baseRecords = getTokenRecordsFromFile(filenameBase);
   const branchRecords = getTokenRecordsFromFile(filenameBranch);
 
-  compareMarketValue(baseRecords, branchRecords, comparisonFile);
-  compareLiquidBacking(baseRecords, branchRecords, comparisonFile);
+  compareMarketValueRecords(baseRecords, branchRecords, comparisonFile);
+  compareLiquidBackingRecords(baseRecords, branchRecords, comparisonFile);
 
-  writeComparisonFile(comparisonFile);
+  // Get TokenSupply and OHM price
+  const subgraphId = comparisonFile.branches.branch.subgraphId;
+  const block = comparisonFile.latestBlock;
+  getTokenSupplies(subgraphId, block).then((branchTokenSupplies) => {
+    getOhmPrice(subgraphId, block).then((ohmPrice) => {
+      doLiquidBackingCheck(branchRecords, branchTokenSupplies, ohmPrice, comparisonFile);
+
+      writeComparisonFile(comparisonFile);
+    })
+  })
 };
 
 const parseSubgraphId = (value: string, _previous: string): string => {
