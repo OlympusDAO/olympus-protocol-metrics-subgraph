9d165e689e7935d0f96baa8f78f916978c261631
Jem <0x0xjem@gmail.com>
2022-09-13 13:34:47 +0100

    Add POL to Polygon

--- networks/polygon/src/contracts/Constants.ts
+++ networks/polygon/src/contracts/Constants.ts
@@ -1,4 +1,4 @@
-import { TokenCategoryStable, TokenCategoryVolatile, TokenDefinition } from "../../../shared/src/contracts/TokenDefinition";
+import { TokenCategoryPOL, TokenCategoryStable, TokenCategoryVolatile, TokenDefinition } from "../../../shared/src/contracts/TokenDefinition";
 import { AAVE_ALLOCATOR, AAVE_ALLOCATOR_V2, BALANCER_ALLOCATOR, BONDS_DEPOSIT, BONDS_INVERSE_DEPOSIT, CONVEX_ALLOCATOR1, CONVEX_ALLOCATOR2, CONVEX_ALLOCATOR3, CONVEX_CVX_ALLOCATOR, CONVEX_CVX_VL_ALLOCATOR, CROSS_CHAIN_ARBITRUM, CROSS_CHAIN_FANTOM, CROSS_CHAIN_POLYGON, DAO_WALLET, LUSD_ALLOCATOR, RARI_ALLOCATOR, TREASURY_ADDRESS_V1, TREASURY_ADDRESS_V2, TREASURY_ADDRESS_V3, VEFXS_ALLOCATOR } from "../../../shared/src/Wallets";
 
 export const BLOCKCHAIN = "Polygon";
@@ -28,6 +28,8 @@ ERC20_TOKENS_POLYGON.set(ERC20_KLIMA, new TokenDefinition(ERC20_KLIMA, TokenCate
 ERC20_TOKENS_POLYGON.set(ERC20_SYN, new TokenDefinition(ERC20_SYN, TokenCategoryVolatile, true, false));
 ERC20_TOKENS_POLYGON.set(ERC20_USDC, new TokenDefinition(ERC20_USDC, TokenCategoryStable, true, false));
 ERC20_TOKENS_POLYGON.set(ERC20_WETH, new TokenDefinition(ERC20_WETH, TokenCategoryVolatile, true, true));
+
+ERC20_TOKENS_POLYGON.set(LP_UNISWAP_V2_WETH_GOHM, new TokenDefinition(LP_UNISWAP_V2_WETH_GOHM, TokenCategoryPOL, true, false));
 
 export const OHM_TOKENS = [ERC20_GOHM];
 
