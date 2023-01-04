d112df3a2984b87bc56467a15108065d25e23d5a
Jem <0x0xjem@gmail.com>
2023-01-04 17:35:36 +0400

    Add separate subgraph for PriceSnapshot
    Shift getCurrentIndex to shared code

--- subgraphs/price-snapshot/src/PriceSnapshot.ts
+++ subgraphs/price-snapshot/src/PriceSnapshot.ts
@@ -1,9 +1,9 @@
 import { BigInt, ethereum } from "@graphprotocol/graph-ts";
 
-import { getCurrentIndex } from "../../../shared/src/supply/OhmCalculations";
-import { getISO8601StringFromTimestamp } from "../../../shared/src/utils/DateHelper";
-import { PriceSnapshot } from "../../generated/schema";
-import { getBaseOhmUsdRate } from "../utils/Price";
+import { getBaseOhmUsdRate } from "../../ethereum/src/utils/Price";
+import { getCurrentIndex } from "../../shared/src/supply/OhmCalculations";
+import { getISO8601StringFromTimestamp } from "../../shared/src/utils/DateHelper";
+import { PriceSnapshot } from "../generated/schema";
 
 export function handleBlock(block: ethereum.Block): void {
     const ohmIndex = getCurrentIndex(block.number);
---
+++ subgraphs/shared/src/Constants.ts
@@ -0,0 +1,4 @@
+export const ERC20_SOHM_V2 = "0x04f2694c8fcee23e8fd0dfea1d4f5bb8c352111f".toLowerCase();
+export const ERC20_SOHM_V2_BLOCK = "12622596";
+export const ERC20_SOHM_V3 = "0x04906695D6D12CF5459975d7C3C03356E4Ccd460".toLowerCase();
+export const ERC20_SOHM_V3_BLOCK = "13806000";
