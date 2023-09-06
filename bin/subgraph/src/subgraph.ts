84947d5caf23edee505b12a27621ac4e9c5a2704
Jem <0x0xjem@gmail.com>
2023-09-06 15:59:33 +0400

    Consolidate on node-fetch instead of using cross-fetch also

--- bin/subgraph/src/subgraph.ts
+++ bin/subgraph/src/subgraph.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, gql, HttpLink, InMemoryCache } from "@apollo/client/core";
-import { fetch } from "cross-fetch";
+import { fetch } from "node-fetch";
 
 const performQuery = async (subgraphId: string, query: string): Promise<any> => {
   const SUBGRAPH_BASE = "https://api.thegraph.com/subgraphs/id/";
