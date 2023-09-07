ca936e7b2f9bea7425ff595601cd00a40fd99998
Jem <0x0xjem@gmail.com>
2023-09-07 11:18:44 +0400

    Unused import

--- subgraphs/ethereum/tests/erc4626.test.ts
+++ subgraphs/ethereum/tests/erc4626.test.ts
@@ -5,7 +5,7 @@ import { toBigInt, toDecimal } from "../../shared/src/utils/Decimals";
 import { TREASURY_ADDRESS_V3 } from "../../shared/src/Wallets";
 import { getAllERC4626Balances } from "../src/utils/ERC4626";
 import { mockWalletBalance, mockZeroWalletBalances } from "./walletHelper";
-import { ERC20_WBTC, getWalletAddressesForContract } from "../src/utils/Constants";
+import { getWalletAddressesForContract } from "../src/utils/Constants";
 
 const SDAI = "0x83F20F44975D03b1b09e64809B757c47f942BEeA";
 const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
