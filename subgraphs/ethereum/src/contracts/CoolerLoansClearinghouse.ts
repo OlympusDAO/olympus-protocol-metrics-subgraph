01821199d01eb5a6b6a95287546e6ffa4c2b73e3
Jem <0x0xjem@gmail.com>
2023-09-22 13:45:18 +0400

    Report the Cooler Loans Clearinghouse balance in assets

---
+++ subgraphs/ethereum/src/contracts/CoolerLoansClearinghouse.ts
@@ -0,0 +1,42 @@
+import { Address, BigInt, log } from "@graphprotocol/graph-ts";
+import { TokenRecord } from "../../../shared/generated/schema";
+import { CoolerLoansClearinghouse } from "../../generated/ProtocolMetrics/CoolerLoansClearinghouse";
+import { COOLER_LOANS_CLEARINGHOUSE } from "../../../shared/src/Wallets";
+import { createOrUpdateTokenRecord } from "../../../shared/src/utils/TokenRecordHelper";
+import { BLOCKCHAIN, ERC20_DAI, ERC20_TOKENS, getContractName } from "../utils/Constants";
+import { getUSDRate } from "../utils/Price";
+import { toDecimal } from "../../../shared/src/utils/Decimals";
+
+export function getClearinghouseBalance(timestamp: BigInt, blockNumber: BigInt): TokenRecord[] {
+  const records: TokenRecord[] = [];
+
+  // Grab the receivables from the clearinghouse
+  const clearinghouseContract = CoolerLoansClearinghouse.bind(Address.fromString(COOLER_LOANS_CLEARINGHOUSE));
+  const receivablesResult = clearinghouseContract.try_principalReceivables();
+
+  if (receivablesResult.reverted) {
+    return records;
+  }
+
+  const daiRate = getUSDRate(ERC20_DAI, blockNumber);
+  const receivablesBalance = toDecimal(receivablesResult.value, 18);
+  log.info(`Cooler Loans Clearinghouse receivables balance: {}`, [receivablesBalance.toString()]);
+
+  records.push(
+    createOrUpdateTokenRecord(
+      timestamp,
+      `${getContractName(ERC20_DAI)} - Receivables`,
+      ERC20_DAI,
+      getContractName(COOLER_LOANS_CLEARINGHOUSE),
+      COOLER_LOANS_CLEARINGHOUSE,
+      daiRate,
+      receivablesBalance,
+      blockNumber,
+      true, // Considers DAI receivables as liquid
+      ERC20_TOKENS,
+      BLOCKCHAIN,
+    ),
+  );
+
+  return records;
+}
--- subgraphs/ethereum/src/utils/TreasuryCalculations.ts
+++ subgraphs/ethereum/src/utils/TreasuryCalculations.ts
@@ -16,6 +16,7 @@ import {
 import { getStablecoinBalances } from "./TokenStablecoins";
 import { getVolatileTokenBalances } from "./TokenVolatile";
 import { getAllERC4626Balances } from "./ERC4626";
+import { getClearinghouseBalance } from "../contracts/CoolerLoansClearinghouse";
 
 /**
  * Returns the market value, which is composed of:
@@ -53,6 +54,12 @@ export function generateTokenRecords(timestamp: BigInt, blockNumber: BigInt): To
   pushTokenRecordArray(
     records,
     getAllERC4626Balances(timestamp, blockNumber),
+  );
+
+  // Get Clearinghouse balances
+  pushTokenRecordArray(
+    records,
+    getClearinghouseBalance(timestamp, blockNumber),
   );
 
   return records;
