11e53981215509a9d4f8cd10e8f531ae5c7dbaf0
Jem <0x0xjem@gmail.com>
2025-01-28 15:37:21 +0400

    chore: linting

--- subgraphs/base/src/price/PriceChainlink.ts
+++ subgraphs/base/src/price/PriceChainlink.ts
@@ -2,7 +2,7 @@ import { Address, BigDecimal } from "@graphprotocol/graph-ts";
 
 import { toDecimal } from "../../../shared/src/utils/Decimals";
 import { ChainlinkPriceFeed } from "../../generated/TokenRecords-base/ChainlinkPriceFeed";
-import { ERC20_WETH, ERC20_USDC } from "../contracts/Constants";
+import { ERC20_USDC,ERC20_WETH } from "../contracts/Constants";
 
 const tokenPriceFeedMap: Map<string, string> = new Map<string, string>();
 tokenPriceFeedMap.set(ERC20_WETH, "0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70".toLowerCase());
